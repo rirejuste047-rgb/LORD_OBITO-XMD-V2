a59aead1d42f4e14c473ba5ebde943dbe9a2f4fb
👑LORD OBITO 👑 <jnguessan672@gmail.com>
2025-07-17 14:23:50 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
   MODE: 'public', // public or private
-  SESSION_ID: process.env.SESSION_ID || 'PLEASE ADD YOUR SESSION HERE LORD~OBITO~',
+  SESSION_ID: process.env.SESSION_ID || 'LORD~OBITO~OB13SCqJ#MtPILUHyFzdgbgPCNjsghoShff_Yq9FHkhoP1MD-ogQ',
   OWNER_NUMBER: 'Wa.me/2250712668494',
   OWNER_NAME: '✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞',
   CHANNEL_URL: 'https://whatsapp.com/channel/0029Vb65HSyHwXbEQbQjQV26',
