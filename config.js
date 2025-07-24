4e785e11411d409a8436841ef872bbb9b6a6a4e7
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-07-24 14:04:32 +0000

    Add files via upload

--- config.js
+++ config.js
@@ -1,13 +1,24 @@
-export default {
-MODE: 'private', // public or private
-SESSION_ID: process.env.SESSION_ID || 'LORD~OBITO~WMMXjCQQ#upYAfH9leBVnaG886I_j6bxDGYYE34CDaVBFuNcMl7g',
-OWNER_NUMBER: '2250712668494',
-OWNER_NAME: '✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞',
-CHANNEL_URL: 'https://whatsapp.com/channel/0029Vb65HSyHwXbEQbQjQV26',
-AUTO_REACT: false,
-PREFIX: '.', // <-- here is your command prefix
-BOT_NAME: 'LORD_OBITO-XMD-V2', // Nom du bot utilisé dans les messages stylés
-WELCOME_ENABLED: false, // Active/désactive la commande welcome
-GOODBYE_ENABLED: false // Active/désactive la commande goodbye
-};
+import fs from 'fs-extra';
 
+// Charger dynamiquement le MODE depuis mode.json
+let MODE = 'private'; // Valeur par d��faut si le fichier ne se charge pas
+
+try {
+  const modeData = await fs.readJSON('./lib/mode.json');
+  MODE = modeData.mode || 'private';
+} catch (err) {
+  console.warn('?? Erreur lors du chargement du mode.json, utilisation du mode par d��faut (private)');
+}
+
+export default {
+  MODE, // �� maintenant dynamique
+  SESSION_ID: process.env.SESSION_ID || 'LORD~OBITO~YOUR SESSION ID',
+  OWNER_NUMBER: '2250712668494',
+  OWNER_NAME: 'your name ?',
+  CHANNEL_URL: 'https://whatsapp.com/channel/0029Vb65HSyHwXbEQbQjQV26',
+  AUTO_REACT: false,
+  PREFIX: '.',
+  BOT_NAME: 'LORD_OBITO-XMD-V2',
+  WELCOME_ENABLED: false,
+  GOODBYE_ENABLED: false
+};
