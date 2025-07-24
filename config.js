cdfa4e5609e8b9ff784bec9a20f4a71bfdb5c709
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-07-24 14:13:24 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 import fs from 'fs-extra';
 
 // Charger dynamiquement le MODE depuis mode.json
-let MODE = 'private'; // Valeur par d��faut si le fichier ne se charge pas
+let MODE = 'private'; // Valeur par d¨¦faut si le fichier ne se charge pas
 
 try {
   const modeData = await fs.readJSON('./lib/mode.json');
   MODE = modeData.mode || 'private';
 } catch (err) {
-  console.warn('?? Erreur lors du chargement du mode.json, utilisation du mode par d��faut (private)');
+  console.warn('?? Erreur lors du chargement du mode.json, utilisation du mode par d¨¦faut (private)');
 }
 
 export default {
-  MODE, // �� maintenant dynamique
-  SESSION_ID: process.env.SESSION_ID || 'LORD~OBITO~YOUR SESSION ID',
+  MODE, // ¡û maintenant dynamique
+  SESSION_ID: process.env.SESSION_ID || 'LORD~OBITO~aVNjiY6Q#IOHcCpQBUsdekcVQKFFZb_tJ-OMBvF17TmClJYho8io',
   OWNER_NUMBER: '2250712668494',
-  OWNER_NAME: 'your name ?',
+  OWNER_NAME: '✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞',
   CHANNEL_URL: 'https://whatsapp.com/channel/0029Vb65HSyHwXbEQbQjQV26',
   AUTO_REACT: false,
   PREFIX: '.',
